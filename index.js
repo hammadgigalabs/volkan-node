bb58139d24a2480ed518dc96972e742658d47cca
Hammad Khalid <hammad.khalid@gigalabs.co>
2020-11-10 19:52:53 +0500

    final touches

--- index.js
+++ index.js
@@ -10,145 +10,181 @@ app.get('/users', (req, res) => {
   res.send([
     {
       id: 1,
-      username: 'hammad',
-      name: 'Hammad Khalid',
-      email: 'hammad.khalid@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser1',
+      name: 'Test User 1',
+      email: 'test.user1@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
       },
       favored: false
     },
     {
       id: 2,
-      username: 'zeeshan',
-      name: 'Zeeshan Imran',
-      email: 'zeeshan@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser2',
+      name: 'Test User 2',
+      email: 'test.user2@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
       },
       favored: false
     },
     {
       id: 3,
-      username: 'asfand',
-      name: 'Asfand Yar',
-      email: 'asfand@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser3',
+      name: 'Test User 3',
+      email: 'test.user3@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
       },
       favored: false
     },
     {
       id: 4,
-      username: 'abdullah',
-      name: 'Abdullah Warraich',
-      email: 'abdullah@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser4',
+      name: 'Test User 4',
+      email: 'test.user4@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
       },
       favored: false
     },
     {
       id: 5,
-      username: 'mustafa',
-      name: 'Mustafa Ali',
-      email: 'mustafa.ali@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser5',
+      name: 'Test User 5',
+      email: 'test.user5@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
       },
       favored: false
     },
     {
       id: 6,
-      username: 'wajid',
-      name: 'Wajid Shabbir',
-      email: 'wajid@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser6',
+      name: 'Test User 6',
+      email: 'test.user6@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
       },
       favored: false
     },
     {
       id: 7,
-      username: 'afzaal',
-      name: 'Afzaal Ahmad',
-      email: 'afzaal@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser7',
+      name: 'Test User 7',
+      email: 'test.user7@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
       },
       favored: false
     },
     {
       id: 8,
-      username: 'danyal',
-      name: 'Muhammad Danyal',
-      email: 'danyal@gigalabs.co',
-      phone: '+92-315-446-8944',
-      website: 'http://gigalabs.co',
+      username: 'testuser8',
+      name: 'Test User 8',
+      email: 'test.user8@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
       address: {
         street: 'Address line 1',
         suite: 'Address line 2',
         city: 'Test',
-        zipcode: 1234
+        zipcode: '1234'
       },
       company: {
-        name: 'Gigalabs'
+        name: 'Google'
+      },
+      favored: false
+    },
+    {
+      id: 9,
+      username: 'testuser9',
+      name: 'Test User 9',
+      email: 'test.user9@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
+      address: {
+        street: 'Address line 1',
+        suite: 'Address line 2',
+        city: 'Test',
+        zipcode: '1234'
+      },
+      company: {
+        name: 'Google'
+      },
+      favored: false
+    },
+    {
+      id: 10,
+      username: 'testuser10',
+      name: 'Test User 10',
+      email: 'test.user10@test.co',
+      phone: '+12-345-678-9012',
+      website: 'http://google.co',
+      address: {
+        street: 'Address line 1',
+        suite: 'Address line 2',
+        city: 'Test',
+        zipcode: '1234'
+      },
+      company: {
+        name: 'Google'
       },
       favored: false
     }
